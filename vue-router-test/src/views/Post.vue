0265db6a1d709433d5bf2f10c95029f1e38e1fa6
Monish <monishatom@outlook.com>
2022-08-14 09:42:21 +0000

    Update Post.vue

--- vue-router-test/src/views/Post.vue
+++ vue-router-test/src/views/Post.vue
@@ -9,6 +9,7 @@ const route = useRoute();
 
 <template>
     <div class="post-container">
+        <button @click="router.back()">Go Back</button>
         <h2>
             This is a Post Page
         </h2>
@@ -16,11 +17,25 @@ const route = useRoute();
         <p>
            Post no: {{ route.params.id }}
         </p>
+        
+        <router-link :to="{
+            name: 'Post',
+            params: {
+                id: 2,
+                details: 'Pariatur tenetur earum suscipit aspernatur. Voluptas et perferendis dolorum.'
+            }
+        }"> Post Details </router-link>
+        
     </div>
 </template>
 
 <style scoped>
 h2 {
     margin-top: 8px;
+}
+
+button {
+    margin-top: 8px;
+    color: #fff;
 }
 </style>
