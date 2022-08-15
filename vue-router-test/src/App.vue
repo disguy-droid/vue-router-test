b01b9fb79f500c70d24b6b3526e37c751a93ab05
Monish <monishatom@outlook.com>
2022-08-15 08:09:41 +0000

    Update 10 files

--- vue-router-test/src/App.vue
+++ vue-router-test/src/App.vue
@@ -2,13 +2,15 @@
 </script>
 
 <template>
-  <div id="app">
-      <router-link to="/" class="router-link">
-          Home
-      </router-link> |
-      <router-link to="/about" class="router-link">
-          About
-      </router-link>
+  <div id="app" class="h-screen w-full flex flex-col items-center font-clashGrotesk">
+      <div class="py-8">
+          <router-link to="/" class="router-link">
+            Home
+          </router-link> ~
+          <router-link to="/about" class="router-link">
+            About
+          </router-link>
+      </div>
       
     <router-view>
     </router-view>
@@ -16,23 +18,4 @@
 </template>
 
 <style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-#app {
-    min-height: 100vh;
-    width: 100%;
-    background-color: #fff;
-    color: #000;
-}
-
-.router-link {
-    color: #000;
-    text-decoration: none;
-}
-
-
 </style>
--- vue-router-test/src/views/Home.vue
+++ vue-router-test/src/views/Home.vue
@@ -1,40 +1,42 @@
 <script setup lang="ts">
+import autoAnimate from '@formkit/auto-animate'
+import { ref, onMounted } from 'vue'
+
+const target = ref()
+
+onMounted(() => {
+    autoAnimate(target.value)
+})
+
+
+    
+
 </script>
 
 <template>
-    <div class="home-container">
-        <h2>
+    <div class="bg-gray-50 min-h-screen w-full font-clashGrotesk">
+        <h2 class="text-2xl font-medium text-center mt-8">
             Hello Vue - This is Home Page.
         </h2>
-        
-        <div class="post-container">
-            
+        <div class="post-container flex justify-center" >
             <router-link :to="{
                 name: 'Post',
                 params: {
                     id: 1
                 }
-            }" class="router-link">First Post</router-link>
+            }" class="router-link text-blue-500 mt-8">First Post</router-link>
         </div>
+        <div ref="target" class="mt-8 w-full grid grid-cols-1 md:grid-cols-4 gap-8 px-8">
+            <div id="card" class="bg-gray-300 py-8 px-4 rounded-lg" v-for="data in 10">
+                <h1 class="text-2xl font-bold text-neutral-900 uppercase">Title</h1>
+                <p class="mt-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+        </div>
+        
     </div>
 </template>
 
 <style scoped>
-.home-container {
-    height: 100vh;
-    width: 100%;
-}
-
-h2 {
-    margin-top: 8px;
-}
-
-.post-container {
-    margin-top: 8px;
-}
-
-
-.router-link {
-    text-decoration: none;
-}
 </style>
